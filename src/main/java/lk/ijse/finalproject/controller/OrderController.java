b964aef23423fb76120d1c96f12f35dd802da77b
Ahamed Muiz <ahamedmuiz119@gmail.com>
2025-05-11 17:00:52 +0530

    Initial commit

--- src/main/java/lk/ijse/finalproject/controller/OrderController.java
+++ src/main/java/lk/ijse/finalproject/controller/OrderController.java
@@ -1,4 +1,89 @@
 package lk.ijse.finalproject.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class OrderController {
+
+    @FXML
+    private ComboBox<?> cmbCustomerId;
+
+    @FXML
+    private ComboBox<?> cmbItemId;
+
+    @FXML
+    private TableColumn<?, ?> colAction;
+
+    @FXML
+    private TableColumn<?, ?> colItemId;
+
+    @FXML
+    private TableColumn<?, ?> colItemName;
+
+    @FXML
+    private TableColumn<?, ?> colQuantity;
+
+    @FXML
+    private TableColumn<?, ?> colTotal;
+
+    @FXML
+    private TableColumn<?, ?> colUnitPrice;
+
+    @FXML
+    private Label lblCustomerName;
+
+    @FXML
+    private Label lblItemName;
+
+    @FXML
+    private Label lblItemPrice;
+
+    @FXML
+    private Label lblItemQty;
+
+    @FXML
+    private Label lblOrderDate;
+
+    @FXML
+    private Label lblOrderId;
+
+    @FXML
+    private Label lblTotal;
+
+    @FXML
+    private TableView<?> tblCart;
+
+    @FXML
+    private TextField txtAddToCartQty;
+
+    @FXML
+    void btnAddToCartOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrderOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnResetOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cmbCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void cmbItemOnAction(ActionEvent event) {
+
+    }
+
 }
