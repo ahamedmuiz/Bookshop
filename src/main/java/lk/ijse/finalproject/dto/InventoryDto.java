8d98b2ee755d16c494203f079d545cd3bc36879a
Ahamed Muiz <ahamedmuiz119@gmail.com>
2025-05-19 20:09:48 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/finalproject/dto/InventoryDto.java
@@ -0,0 +1,21 @@
+package lk.ijse.finalproject.dto;
+
+import java.time.LocalDate;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class InventoryDto {
+        private int invId;
+        private int supId;
+        private String supplierName;
+        private int stockQty;
+        private LocalDate lastUpdate;
+        private String category;
+        private double price;
+}
+
+
